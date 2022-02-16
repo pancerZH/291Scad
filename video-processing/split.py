4c8d58d2c392738c34ddd26c9e667ffb5cc3830f
Ubuntu <ubuntu@ip-172-31-18-114.us-west-1.compute.internal>
2022-02-16 22:58:19 +0000

    remove unnecessary components

--- video-processing/split.py
+++
@@ -1,7 +0,0 @@
-import ffmpeg
-
-split = ffmpeg.input('sample-mp4-file.mp4').filter_multi_output('split', 'split=5')
-split0 = split.stream(0) 
-split1 = split[1]
-split2 = split[2]
-ffmpeg.concat(split0, split1, split2).output('out.mp4').run()
