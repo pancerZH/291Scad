4ba517532e36e1548cbe9ff0338811dd298ad90f
Ubuntu <ubuntu@ip-172-31-18-114.us-west-1.compute.internal>
2022-03-06 19:40:17 +0000

    add local bash script

--- video-processing/run.sh
+++ video-processing/run.sh
@@ -4,9 +4,9 @@
 ../../Scad/runtime/lib/memory_server "2335">&/dev/null &
 ../../Scad/runtime/lib/memory_server "2336">&/dev/null &
 
-rundisagg func1.o.py -m "mem1:2333" "mem2:2334" "mem3:2335" "mem4:2336">&/dev/null 
-rundisagg func2.o.py -m "mem1:2333" "mem2:2334" "mem3:2335" "mem4:2336">&/dev/null 
-rundisagg func3.o.py -m "mem1:2333" "mem2:2334" "mem3:2335" "mem4:2336">&/dev/null
+mprof run rundisagg func1.o.py -m "mem1:2333" "mem2:2334" "mem3:2335" "mem4:2336">&/dev/null 
+mprof run rundisagg func2.o.py -m "mem1:2333" "mem2:2334" "mem3:2335" "mem4:2336">&/dev/null 
+mprof run rundisagg func3.o.py -m "mem1:2333" "mem2:2334" "mem3:2335" "mem4:2336">&/dev/null
 
 kill -9 $(lsof -i:2333 -t)
 kill -9 $(lsof -i:2334 -t)
---
+++ video-processing/run_local.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+python func1.py
+python func2.py
+python func3.py
