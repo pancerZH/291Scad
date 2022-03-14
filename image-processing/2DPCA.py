e6f6670a172377db2f310b9f8bfb30e544fef101
Zhouyang Li <zhl076@ucsd.edu>
2022-03-13 23:51:34 -0700

    Modify the program we want to split to make it more clear

--- image-processing/2DPCA.py
+++ image-processing/2DPCA.py
@@ -1,8 +1,10 @@
 import numpy as np
 from PIL import Image
+from memory_profiler import profile
 '''
 imgs 是三维的图像矩阵，第一维是图像的个数
 '''
+@profile
 def TwoDPCA(imgs, dim):
     a,b,c = imgs.shape
     average = np.zeros((b,c))
@@ -17,54 +19,68 @@ def TwoDPCA(imgs, dim):
     # print('v_shape:{}'.format(v.shape))
     w = w[::-1]
     v = v[::-1]
-    '''
-    for k in range(c):
-        # alpha = sum(w[:k])*1.0/sum(w)
-        alpha = 0
-        if alpha >= p:
-            u = v[:,:k]
-            break
-    '''
-    print('alpha={}'.format(sum(w[:dim]*1.0/sum(w))))
+
     u = v[:,:dim]
-    print('u_shape:{}'.format(u.shape))
-    return u  # u是投影矩阵
+    return u
 
 
-def TTwoDPCA(imgs, dim):
-    u = TwoDPCA(imgs, dim)
-    a1,b1,c1 = imgs.shape
+@profile
+def TTwoDPCA(imgs, u, dim):
+    a1, b1, c1 = imgs.shape
     img = []
     for i in range(a1):
         temp1 = np.dot(imgs[i,:,:],u)
         img.append(temp1.T)
     img = np.array(img)
     uu = TwoDPCA(img, dim)
-    print('uu_shape:{}'.format(uu.shape))
-    return u,uu  # uu是投影矩阵
+    return uu
 
 
-def image_2D2DPCA(images, u, uu):
-    a, b, c = images.shape
-    new_images = np.ones((a, uu.shape[1], u.shape[1]))
+@profile
+def image_2D2DPCA(image, u, uu):
+    a, b, c = image.shape
+    new_image = np.ones((a, uu.shape[1], u.shape[1]))
     for i in range(a):
-        Y = np.dot(uu.T, images[i,:,:])
+        Y = np.dot(uu.T, image[i,:,:])
         Y = np.dot(Y, u)
-        new_images[i,:,:] = Y
-    return new_images
+        new_image[i,:,:] = Y
+    return new_image
 
 
-if __name__ == '__main__':
-    im = Image.open('./bloodborne2.jpg')
+@profile
+def process_image(im):
     im_grey = im.convert('L')
     # im_grey.save('a.png')
     a, b = np.shape(im_grey)
     data = im_grey.getdata()
     data = np.array(data)
     data2 = data.reshape(1, a, b)
-    print('data2_shape:{}'.format(data2.shape))
-    u, uu = TTwoDPCA(data2, 10)
+    return data2
+
+
+@profile
+def save_new_image(new_image):
+    result_path = "new-image.jpg"
+
+    # Save the new image
+    a,b,c = new_image.shape
+    new_image = new_image.reshape(b, c)
+    new_im = Image.fromarray(new_image , 'L')
+    new_im.save(result_path)
+
+
+if __name__ == '__main__':
+    im = Image.open('./image.jpg')
+
+    image_data = process_image(im)
+    print('data2_shape:{}'.format(image_data.shape))
+
+    u = TwoDPCA(image_data, 10)
     print('data2_2DPCA_u:{}'.format(u.shape))
+
+    uu = TTwoDPCA(image_data, u, 10)
     print('data2_2D2DPCA_uu:{}'.format(uu.shape))
-    new_images = image_2D2DPCA(data2, u, uu)
-    print('new_images:{}'.format(new_images.shape))
+
+    new_image = image_2D2DPCA(image_data, u, uu)
+    print('new_images:{}'.format(new_image.shape))
+    save_new_image(new_image)
